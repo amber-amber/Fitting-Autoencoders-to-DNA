6ff282b4a87257e181a8597f96371207abe85a6a
amber-amber <amberyuan678@gmail.com>
2017-06-26 13:09:48 -0500

    Add files via upload

---
+++ Basic_DNA_Autoencoder.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras import layers
+
+
+dna_data = pd.read_csv('coreseed.train.tsv', names=["dna","protein"], usecols=[5,6], delimiter ='\t', header =0)
+n,m=dna_data.shape
+dna_data.dna=dna_data.dna.str[:10]
+
+class CharacterTable(object):
+    def __init__(self, chars):
+        self.chars=sorted(set(chars))
+        self.char_indices=dict((c,i) for i,c in enumerate(self.chars))
+        self.indices_char=dict((i,c) for i,c in enumerate(self.chars))
+
+    def encode(self, C, num_rows):
+        x = np.zeros((num_rows,len(self.chars)))
+        for i,c in enumerate(C):
+            x[i, self.char_indices[c]]=1
+        return x
+
+    def decode(self, x, calc_argmax=True):
+        if calc_argmax:
+            x = x. argmax(axis=-1)
+        return ''.join(self.indices_char[x] for x in x)
+
+class colors:
+    ok='\033[92m'
+    fail= '\033[91m'
+    close='\033[0m'
+
+chars='actg'
+ctable= CharacterTable(chars)
+
+MAXLEN = 10
+
+print('Vectorization... vectorization... vectorization')
+x=np.zeros((n,MAXLEN,len(chars)),dtype=np.bool)
+print('shape of vector: ' ,x.shape)
+#y=x=np.zeros((n,MAXLEN,len(chars)),dtype=np.bool)
+for i, dna_str in enumerate(dna_data.dna):
+    x[i]=ctable.encode(dna_str,MAXLEN)
+    if i<2:
+        print('dna: ', dna_str, x[i])
+
+split_at = int(.9*n)
+dna_train, dna_test= x[:split_at], x[split_at:]
+
+print('Training Data:')
+print(dna_train.shape)
+
+print('Testing Data:')
+print(dna_test.shape)
+
+RNN = layers.LSTM
+#RNN = layers.SimpleRNN
+HIDDEN_SIZE=128
+BATCH_SIZE=128
+LAYERS=1
+
+print('Build Model... if I am lucky')
+model=Sequential()
+model.add(RNN(HIDDEN_SIZE,input_shape=(MAXLEN,len(chars))))
+#model.add(layers.RepeatVector(1))
+#for _ in range(LAYERS):
+#    model.add(RNN(HIDDEN_SIZE,return_sequences=True))
+
+#model.add(layers.TimeDistributed(layers.Dense(len(chars))))
+#model.add(layers.Activation('softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.summary()
+model.fit(dna_train, dna_train, batch_size=BATCH_SIZE, epochs=1, validation_data=(dna_test, dna_test))
+
+# for iteration in range(20):
+#     print()
+#     print('-'*50)
+#     print('Iteration',iteration)
+#     model.fit(dna_train, dna_train, batch_size=BATCH_SIZE, epochs=1, validation_data=(dna_test, dna_test))
+#     for i in range(10):
+#         ind=np.random.randit(0, len(dna_test))
+#         rowx, rowy = dna_test[np.array([ind])], dna_test[np.array([ind])]
+#         preds=model.predict_classes(rowx, verbose=0)
+#         correct = ctable.decode(rowx[0])
+#         guess = ctable.decode(pres[0],calc_argmax=False)
+#         print('Q', q[::-1] if INVERT else q)
+#         print('T', correct)
+#         if correct == guess:
+#             print(colors.ok + '☑' + colors.close)
+#         else:
+#             print(colors.fail + '☒' + colors.close)
+#         print(guess)
+#         print('---')
