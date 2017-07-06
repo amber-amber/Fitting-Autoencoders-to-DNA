69c9b2553c5843cf120191f520262c5aa88d4192
amber-amber <amberyuan678@gmail.com>
2017-07-06 12:39:37 -0500

    Trying out the keras variational autoencoder...

--- Basic_DNA_Autoencoder.py
+++ Basic_DNA_Autoencoder.py
@@ -31,11 +31,11 @@ class colors:
 chars='actg'
 ctable= CharacterTable(chars)
 
-#MAXLEN=25
+MAXLEN=25
 n_rows = 50000
 dna_data = pd.read_csv('coreseed.train.tsv', names=["dna","protein"], usecols=[5,6], nrows= n_rows, delimiter ='\t', header =0)
 n,m=dna_data.shape
-for MAXLEN in range(30,100,10):
+#for MAXLEN in range(30,100,10):
     print('The number of bases in the DNA is ', MAXLEN)
     print('\n')
     dna_data.dna=dna_data.dna.str[:MAXLEN]
@@ -45,71 +45,72 @@ for MAXLEN in range(30,100,10):
     print('shape of vector: ' ,x.shape)
     for i, dna_str in enumerate(dna_data.dna):
         x[i]=ctable.encode(dna_str,MAXLEN)
+    print x[1]
 
-    split_at = int(.9*n)
-    dna_train, dna_test= x[:split_at], x[split_at:]
-
-    print('Training Data:')
-    print(dna_train.shape)
-
-    print('Testing Data:')
-    print(dna_test.shape)
-
-    RNN = layers.LSTM
-    #RNN = layers.SimpleRNN
-    HIDDEN_SIZE=128
-    BATCH_SIZE=128
-    LAYERS=1
-
-    print('Build Model...')
-    model=Sequential()
-    model.add(RNN(HIDDEN_SIZE,input_shape=(MAXLEN,len(chars))))
-    model.add(layers.RepeatVector(MAXLEN))
-    for _ in range(LAYERS):
-        model.add(RNN(HIDDEN_SIZE,return_sequences=True))
-
-    model.add(layers.TimeDistributed(layers.Dense(len(chars))))
-    model.add(layers.Activation('softmax'))
-    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-    #model.compile(loss='MSE',optimizer='adam',metrics=['accuracy'])
-    model.summary()
-    #model.fit(dna_train, dna_train, batch_size=BATCH_SIZE, epochs=1, validation_data=(dna_test, dna_test))
-    # for i in range(10):
-    #        ind=np.random.randint(0, len(dna_test))
-    #        rowx, rowy = dna_test[np.array([ind])], dna_test[np.array([ind])]
-    #        preds=model.predict_classes(rowx, verbose=0)
-    #        q = ctable.decode(rowx[0])
-    #        correct = ctable.decode(rowx[0])
-    #        guess = ctable.decode(preds[0],calc_argmax=False)
-    #        print('Q', q[::-1] )
-    #        print('T', correct)
-    #        if correct == guess:
-    #            print(colors.ok + '☑' + colors.close)
-    #        else:
-    #            print(colors.fail + '☒' + colors.close)
-    #        print(guess)
-    #        print('---')
-
-    def main(epochs):
-        print()
-        print('-'*50)
-        model.fit(dna_train, dna_train, batch_size=BATCH_SIZE, epochs=epochs, validation_data=(dna_test, dna_test))
-        for i in range(10):
-            ind=np.random.randint(0, len(dna_test))
-            rowx, rowy = dna_test[np.array([ind])], dna_test[np.array([ind])]
-            preds=model.predict_classes(rowx, verbose=0)
-            q = ctable.decode(rowx[0])
-            correct = ctable.decode(rowx[0])
-            guess = ctable.decode(preds[0],calc_argmax=False)
-            print('Q', q[::-1])
-            print('T', correct)
-            if correct == guess:
-                print(colors.ok + '☑' + colors.close)
-            else:
-                print(colors.fail + '☒' + colors.close)
-            print(guess)
-            print('---')
-
-    if __name__ == '__main__':
-            args = sys.argv[1:]
-            main(int(args[0]))
+    # split_at = int(.9*n)
+    # dna_train, dna_test= x[:split_at], x[split_at:]
+    #
+    # print('Training Data:')
+    # print(dna_train.shape)
+    #
+    # print('Testing Data:')
+    # print(dna_test.shape)
+    #
+    # RNN = layers.LSTM
+    # #RNN = layers.SimpleRNN
+    # HIDDEN_SIZE=128
+    # BATCH_SIZE=128
+    # LAYERS=1
+    #
+    # print('Build Model...')
+    # model=Sequential()
+    # model.add(RNN(HIDDEN_SIZE,input_shape=(MAXLEN,len(chars))))
+    # model.add(layers.RepeatVector(MAXLEN))
+    # for _ in range(LAYERS):
+    #     model.add(RNN(HIDDEN_SIZE,return_sequences=True))
+    #
+    # model.add(layers.TimeDistributed(layers.Dense(len(chars))))
+    # model.add(layers.Activation('softmax'))
+    # model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    # #model.compile(loss='MSE',optimizer='adam',metrics=['accuracy'])
+    # model.summary()
+    # #model.fit(dna_train, dna_train, batch_size=BATCH_SIZE, epochs=1, validation_data=(dna_test, dna_test))
+    # # for i in range(10):
+    # #        ind=np.random.randint(0, len(dna_test))
+    # #        rowx, rowy = dna_test[np.array([ind])], dna_test[np.array([ind])]
+    # #        preds=model.predict_classes(rowx, verbose=0)
+    # #        q = ctable.decode(rowx[0])
+    # #        correct = ctable.decode(rowx[0])
+    # #        guess = ctable.decode(preds[0],calc_argmax=False)
+    # #        print('Q', q[::-1] )
+    # #        print('T', correct)
+    # #        if correct == guess:
+    # #            print(colors.ok + '☑' + colors.close)
+    # #        else:
+    # #            print(colors.fail + '☒' + colors.close)
+    # #        print(guess)
+    # #        print('---')
+    #
+    # def main(epochs):
+    #     print()
+    #     print('-'*50)
+    #     model.fit(dna_train, dna_train, batch_size=BATCH_SIZE, epochs=epochs, validation_data=(dna_test, dna_test))
+    #     for i in range(10):
+    #         ind=np.random.randint(0, len(dna_test))
+    #         rowx, rowy = dna_test[np.array([ind])], dna_test[np.array([ind])]
+    #         preds=model.predict_classes(rowx, verbose=0)
+    #         q = ctable.decode(rowx[0])
+    #         correct = ctable.decode(rowx[0])
+    #         guess = ctable.decode(preds[0],calc_argmax=False)
+    #         print('Q', q[::-1])
+    #         print('T', correct)
+    #         if correct == guess:
+    #             print(colors.ok + '☑' + colors.close)
+    #         else:
+    #             print(colors.fail + '☒' + colors.close)
+    #         print(guess)
+    #         print('---')
+    #
+    # if __name__ == '__main__':
+    #         args = sys.argv[1:]
+    #         main(int(args[0]))
