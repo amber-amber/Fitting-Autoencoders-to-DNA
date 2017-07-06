76f8c0962298b359fe16ab660bd7b8968a18a678
amber-amber <amberyuan678@gmail.com>
2017-07-06 10:58:03 -0500

    Trying out the keras variational autoencoder...

---
+++ DNA_Base_Gen_LSTM.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout,LSTM
+
+n_rows = 10000
+MAXLEN = 60
+dna_data = pd.read_csv('coreseed.train.tsv', names=dna, usecols=5, nrows= n_rows, delimiter ='\t', header =0)
+#n,m = dna_data.shape
+dna_data.dna=dna_data.dna.str[:MAXLEN]
+print "DNA shape: ", dna_data.shape
+
+#We need to come up with the list of training patterns
+base_in_len = 20
+bases_in = []
+base_out = []
+for i in range(n_rows):
+    for j in range(0, MAXLEN-base_in_len):
+        current_base_in = dna_data.dna[i,j:j+base_in_len]
+        current_base_out = dna_data.dna[i,j+base_in_len]
+        bases_in.append(current_base_in)
+        base_out.append(current_base_out)
+n_patterns = len(bases_in)
+print "Number of patterns: ", n_patterns
