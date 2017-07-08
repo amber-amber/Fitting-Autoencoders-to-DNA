d44096fc75afa34b64f5944d81cbc4280790694e
amber-amber <amberyuan678@gmail.com>
2017-07-08 14:43:55 -0500

    Trying out the keras variational autoencoder...

--- Protein_Gen_LSTM.py
+++ Protein_Gen_LSTM.py
@@ -35,7 +35,9 @@ for i in range(n_rows):
             protein_in.append(current_protein_in)
             protein_out.append(current_protein_out)
 n_patterns = len(protein_in)
-chars = sorted(set(chars))
+chars = sorted(list(set(chars)))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
 print "Number of protein characters: ", len(chars)
 print "Number of patterns: ", n_patterns
 print "Sample of bases in:", protein_in[889]
@@ -43,22 +45,22 @@ print "Sample of base_out:", protein_out[889]
 
 #Vectorization
 #Let's just use one hot encoding because that's all I know fml
-class CharacterTable(object):
-    def __init__(self, chars):
-        self.chars=sorted(set(chars))
-        self.char_indices=dict((c,i) for i,c in enumerate(self.chars))
-        self.indices_char=dict((i,c) for i,c in enumerate(self.chars))
-
-    def encode(self, C, num_rows):
-        x = np.zeros((num_rows,len(self.chars)))
-        for i,c in enumerate(C):
-            x[i, self.char_indices[c]]=1
-        return x
-
-    def decode(self, x, calc_argmax=True):
-        if calc_argmax:
-            x = x. argmax(axis=-1)
-        return ''.join(self.indices_char[x] for x in x)
+# class CharacterTable(object):
+#     def __init__(self, chars):
+#         self.chars=sorted(set(chars))
+#         self.char_indices=dict((c,i) for i,c in enumerate(self.chars))
+#         self.indices_char=dict((i,c) for i,c in enumerate(self.chars))
+#
+#     def encode(self, C, num_rows):
+#         x = np.zeros((num_rows,len(self.chars)))
+#         for i,c in enumerate(C):
+#             x[i, self.char_indices[c]]=1
+#         return x
+#
+#     def decode(self, x, calc_argmax=True):
+#         if calc_argmax:
+#             x = x. argmax(axis=-1)
+#         return ''.join(self.indices_char[x] for x in x)
 
 ctable= CharacterTable(chars)
 
@@ -68,32 +70,38 @@ hot_y=np.zeros((n_patterns, 1, len(chars)),dtype=np.bool)
 print 'Shape of input vector: ', hot_x.shape
 print 'Shape of output vector: ', hot_y.shape
 for i, prot_str in enumerate(protein_in):
-    hot_x[i]=ctable.encode(prot_str, protein_in_len)
-for i, next_prot in enumerate(protein_out):
-    hot_y[i]=ctable.encode(next_prot,1)
+#     hot_x[i]=ctable.encode(prot_str, protein_in_len)
+    for t, char in enumerate(prot_str):
+            hot_x[i,t,char_indices[char]] = 1
+    hot_y[i, char_indices[protein_out[i]]] = 1
+# for i, next_prot in enumerate(protein_out):
+#     hot_y[i]=ctable.encode(next_prot,1)
+print 'example of encoded protein: ' hot_x[8]
+print 'example of encoded next protein: ' hot_y[8]
+
 #print hot_x[1]
 #print hot_y[1]
 
 #The Single Layer LSTM Model
 #
-HIDDEN_SIZE =128
-BATCH_SIZE=128
-LAYERS=1
-
-model = Sequential()
-model.add(layers.LSTM(HIDDEN_SIZE,input_shape=(hot_x.shape[1], hot_x.shape[2])))
-model.add(layers.Dense(len(chars)))
-model.add(layers.Activation('softmax'))
-# model.add(layers.RepeatVector(MAXLEN))
-# for _ in range(LAYERS):
-#     model.add(layers.LSTM(HIDDEN_SIZE, return_sequences=True))
+# HIDDEN_SIZE =128
+# BATCH_SIZE=128
+# LAYERS=1
 #
-# model.add(layers.TimeDistributed(layers.Dense(len(chars))))
+# model = Sequential()
+# model.add(layers.LSTM(HIDDEN_SIZE,input_shape=(hot_x.shape[1], hot_x.shape[2])))
+# model.add(layers.Dense(len(chars)))
 # model.add(layers.Activation('softmax'))
-#model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer)
-
-model.summary()
-
-model.fit(hot_x, hot_y, epochs=5, batch_size=128)
+# # model.add(layers.RepeatVector(MAXLEN))
+# # for _ in range(LAYERS):
+# #     model.add(layers.LSTM(HIDDEN_SIZE, return_sequences=True))
+# #
+# # model.add(layers.TimeDistributed(layers.Dense(len(chars))))
+# # model.add(layers.Activation('softmax'))
+# #model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+# optimizer = RMSprop(lr=0.01)
+# model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+#
+# model.summary()
+#
+# model.fit(hot_x, hot_y, epochs=5, batch_size=128)
