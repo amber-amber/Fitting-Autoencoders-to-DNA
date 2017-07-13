bc4e54958cf0981ca13c4726f74501bccc0aaa9c
amber-amber <amberyuan678@gmail.com>
2017-07-13 16:19:48 -0500

    Delete WtPickle.py

--- WtPickle.py
+++
@@ -1,9 +0,0 @@
-#import numpy
-import gzip, cPickle
-
-mnist = gzip.open('mnist.pkl.gz mnist.pkl.gz','wb')
-train_set, valid_set, test_set = cPickle.load(mnist)
-mnist.close()
-
-print('Training set shape: ', train_set.shape)
-print('Validation set shape: ', valid_set.shape)
