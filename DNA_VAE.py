38a0dae518d7547881d628075a9ac851effedf1f
amber-amber <amberyuan678@gmail.com>
2017-07-17 17:09:19 -0500

    DNA VAE

--- DNA_VAE.py
+++ DNA_VAE.py
@@ -84,29 +84,29 @@ h_decoded = decoder_h(z)
 x_decoded_mean = decoder_mean(h_decoded)
 
 
-# Custom loss layer
-# class CustomVariationalLayer(Layer):
-#     def __init__(self, **kwargs):
-#         self.is_placeholder = True
-#         super(CustomVariationalLayer, self).__init__(**kwargs)
+#Custom loss layer
+class CustomVariationalLayer(Layer):
+    def __init__(self, **kwargs):
+        self.is_placeholder = True
+        super(CustomVariationalLayer, self).__init__(**kwargs)
+
+    def vae_loss(self, x, x_decoded_mean):
+        xent_loss = MAXLEN * metrics.binary_crossentropy(x, x_decoded_mean)
+        kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+        return K.mean(xent_loss + kl_loss)
+
+    def call(self, inputs):
+        x = inputs[0]
+        x_decoded_mean = inputs[1]
+        loss = self.vae_loss(x, x_decoded_mean)
+        self.add_loss(loss, inputs=inputs)
+        # We won't actually use the output.
+        return x
 #
-#     def vae_loss(self, x, x_decoded_mean):
-#         xent_loss = MAXLEN * metrics.binary_crossentropy(x, x_decoded_mean)
-#         kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-#         return K.mean(xent_loss + kl_loss)
-#
-#     def call(self, inputs):
-#         x = inputs[0]
-#         x_decoded_mean = inputs[1]
-#         loss = self.vae_loss(x, x_decoded_mean)
-#         self.add_loss(loss, inputs=inputs)
-#         # We won't actually use the output.
-#         return x
-#
-# y = CustomVariationalLayer()([x, x_decoded_mean])
-# vae = Model(x, y)
-# vae.compile(optimizer='rmsprop', loss=None, metrics=['accuracy'])
-# vae.summary()
+y = CustomVariationalLayer()([x, x_decoded_mean])
+vae = Model(x, y)
+vae.compile(optimizer='rmsprop', loss=None, metrics=['accuracy'])
+vae.summary()
 #
 # #fit to DNA data
 # split_at = int(.9 * n)
