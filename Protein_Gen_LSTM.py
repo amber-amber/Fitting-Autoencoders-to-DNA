20c99861e89ee9e28d7b231c0662443c137bdf6a
amber-amber <amberyuan678@gmail.com>
2017-07-20 17:02:14 -0500

    Increased dropout rate to 0.4

--- Protein_Gen_LSTM.py
+++ Protein_Gen_LSTM.py
@@ -26,6 +26,7 @@ print "DNA shape: ", dna_data.shape
 
 #We need to come up with the list of training patterns
 protein_in_len = 15
+protein_out_len = 1
 step = 5
 protein_in = []
 protein_out = []
@@ -35,7 +36,7 @@ for i in range(n_rows):
          current_protein_in = dna_data.protein[i][j:j+protein_in_len]
          prot_string = str(current_protein_in)
          chars= chars + prot_string
-         if (j+protein_in_len) < (MAXLEN-2):
+         if (j+protein_in_len) < (MAXLEN-protein_out_len):
             current_protein_out = dna_data.protein[i][j+protein_in_len]
             protein_in.append(current_protein_in)
             protein_out.append(current_protein_out)
@@ -88,7 +89,7 @@ HIDDEN_SIZE =128
 BATCH_SIZE=128
 EMBEDDING_DIM = 100
 LAYERS=1
-dropout_rate = 0.2
+dropout_rate = 0.4
 learning_rate = 0.0001
 epochs=50
 
