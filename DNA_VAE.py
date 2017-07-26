aee6d8bfe1d8e9ae1c5953650ac2a4e3d26ad578
amber-amber <amberyuan678@gmail.com>
2017-07-26 16:41:54 -0500

    correlation becomes 2d

--- DNA_VAE.py
+++ DNA_VAE.py
@@ -103,7 +103,7 @@ decoder_h = Dense(intermediate_dim, activation='relu')
 decoder_mean = Dense(original_dim, activation='sigmoid')
 h_decoded = decoder_h(z)
 x_decoded_mean = decoder_mean(h_decoded)
-#print "x_decoded_mean shape: ", x_decoded_mean._keras_shape
+print(x_decoded_mean shape: ", x_decoded_mean._keras_shape
 
 # #Custom loss layer
 
@@ -157,8 +157,15 @@ def vae_loss(y_true, y_pred):
 # def categorical_accuracy(y_true, y_pred):
 #     return K.mean(K.equal(K.argmax(y_true, axis=-1), K.argmax(y_pred, axis=-1)))
 
-def covariance(x, y):
-    return K.mean(x * y) - K.mean(x) * K.mean(y)
+#def covariance(x, y):
+#    return K.mean(x * y) - K.mean(x) * K.mean(y)
+
+def covar_matrix(x,y):
+    return np.cov(x,y)
+
+
+def corr_matrix(x,y):
+    return np.corrcoef(x,y)
 
 def corr(y_true, y_pred):
     cov = covariance(y_true, y_pred)
@@ -183,7 +190,7 @@ learning_rate = 0.0001
 optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
 for_tb = TensorBoard(log_dir='./DNA_VAE',histogram_freq=0, write_graph=True, write_images=True)
-vae.compile(optimizer= optimizer, loss=vae_loss, metrics=[corr, xent])
+vae.compile(optimizer= optimizer, loss=vae_loss, metrics=[corr_matrix])
 #vae.compile(optimizer= optimizer, loss=None, metrics=[corr, xent])
 print('THE VARIATIONAL AUTOENCODER MODEL...')
 vae.summary()
