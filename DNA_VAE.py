7de4118edec58ee9ca73c0d3d663372f4cf2871c
amber-amber <amberyuan678@gmail.com>
2017-07-27 16:18:37 -0500

    added LSTM layer before dense in encoder

--- DNA_VAE.py
+++ DNA_VAE.py
@@ -87,7 +87,7 @@ def sampling(args):
 #for Q(z|X) the encoder
 #this is a neural net with ONE hidden layer
 #x = Input(batch_shape=(batch_size, original_dim))
-x = Input(input_shape=(n_rows, MAXLEN,len(chars)))
+x = Input(shape=(n_rows, MAXLEN,len(chars)))
 print('Input shape: ', x._keras_shape)
 #h = Dense(intermediate_dim, activation='relu')(x)
 h = LSTM(intermediate_dim, input_shape =(MAXLEN,len(chars)))(x)
